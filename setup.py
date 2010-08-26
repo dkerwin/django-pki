fc209ef2bb63bfea26620a59155a728120bf714c
Max Arnold <lwarxx@gmail.com>
2010-08-26 14:27:55 +0700

    setup.py: get version and long_description from appropriate files

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from distutils.core import setup
 
+def get_file_contents(f):
+    try:
+        return open(f, 'rb').read().decode('utf-8').strip()
+    except IOError:
+        return 'UNKNOWN'
+
 setup(
     name = 'django-pki',
     packages = ['pki'],
-    version = '0.10.0',
+    version=get_file_contents('VERSION'),
     description = 'A PKI based on the Django admin',
+    long_description=get_file_contents('README.markdown'),
     author='Daniel Kerwin',
     author_email='daniel@linuxaddicted.de',
     url='http://www.github.com/dkerwin/django-pki/',
+    license='GPL',
     download_url='http://github.com/dkerwin/django-pki/downloads',
     classifiers=[
         'Development Status :: 4 - Beta',
