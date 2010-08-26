1c0e80c099948f49f9e0dbac5495bb767d8f8e99
Max Arnold <lwarxx@gmail.com>
2010-08-26 16:16:08 +0700

    Data files weren't installed, fix this

--- setup.py
+++ setup.py
@@ -1,16 +1,45 @@
+import os
+import sys
+
 from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+from distutils.command.install_data import install_data
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
 
 def get_file_contents(f):
     try:
-        return open(f, 'rb').read().decode('utf-8').strip()
+        return open(os.path.join(root_dir, f), 'rb').read().decode('utf-8').strip()
     except IOError:
         return 'UNKNOWN'
+
+def list_data_files(d):
+    data_files = []
+    for root, dirs, files in os.walk(os.path.join(root_dir, d)):
+        path = root.replace(root_dir + os.sep, '', 1)
+        if len(files) > 0:
+            data_files.append((path, [os.path.join(path, f) for f in files]))
+
+    return data_files
+
+# Tell distutils to put data_files in platform-specific installation
+for scheme in INSTALL_SCHEMES.values(): scheme['data'] = scheme['purelib']
+
+# Fix MacOS platform-specific lib dir
+class osx_install_data(install_data):
+    def finalize_options(self):
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+
+if sys.platform == "darwin":
+    cmdclasses = {'install_data': osx_install_data}
+else:
+    cmdclasses = {'install_data': install_data}
 
 version = get_file_contents('VERSION')
 
 setup(
     name = 'django-pki',
-    packages = ['pki'],
     version=version,
     description = 'A PKI based on the Django admin',
     long_description=get_file_contents('README.markdown'),
@@ -35,4 +64,7 @@ setup(
         'Topic :: Security :: Cryptography',
         'Topic :: System :: Systems Administration',
     ],
+    packages = ['pki'],
+    data_files = list_data_files('pki/media') + list_data_files('pki/templates'),
+    cmdclass = cmdclasses,
 )
